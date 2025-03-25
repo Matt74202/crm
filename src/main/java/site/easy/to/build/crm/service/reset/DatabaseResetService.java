f1a4c621987fa1bc59ecb3bb5f22ada774279f16
Matthew <matthewbogosy@gmail.com>
2025-03-25 14:46:03 +0300

    update reset

--- src/main/java/site/easy/to/build/crm/service/reset/DatabaseResetService.java
+++ src/main/java/site/easy/to/build/crm/service/reset/DatabaseResetService.java
@@ -27,6 +27,8 @@ public class DatabaseResetService {
         entityManager.createNativeQuery("TRUNCATE TABLE ticket_settings").executeUpdate();
         entityManager.createNativeQuery("TRUNCATE TABLE file").executeUpdate();
         entityManager.createNativeQuery("TRUNCATE TABLE google_drive_file").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE budget").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE expense").executeUpdate();
 
         entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
     }
